3352f923eed1e03dde0ad8bf1fbf46dd633e2a7c
max <maxpower2803@gmail.com>
2020-06-15 17:49:39 -0400

    cuarto commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import "./styles/styles.scss"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import MainMenu from "./MainMenu"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          tamos listos ctm
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+
+      <MainMenu />
+      <Switch>
+      <Route path="/" exact component={ MainMenu } />
+      <Route component={() => (
+      <div className="ed-grid">
+      <h1>ERROR 404</h1>
+      </div>)} />
+      </Switch>
+
+  </Router>
+)
 
 export default App;
